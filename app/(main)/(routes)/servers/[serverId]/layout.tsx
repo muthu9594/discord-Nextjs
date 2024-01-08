44ab94214d3a5808af05eea93b539e588c6dedda
muthuBlobcity <muthu.s@blobcity.com>
2024-01-08 17:00:33 +0530

    add dp a,m,s

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,7 +1,10 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
+
 const ServerIdLayout = async({children,params}:{children:React.ReactNode;params:{serverId:string}})=>{
     const profile = await currentProfile();
 
@@ -24,8 +27,14 @@ const ServerIdLayout = async({children,params}:{children:React.ReactNode;params:
         return redirect("/");
     }
     return(
-        <div>
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-60 z-20 fkex-col fixed inset-y-0">
+             <ServerSidebar serverId = {params.serverId}/>
+            </div>
+            <main className="h-full md:pl-60">
             {children}
+
+            </main>
         </div>
     )
 }
