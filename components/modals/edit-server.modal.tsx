24f2b88da049e2bb1107b39aefe8c299a14dd76c
muthuBlobcity <muthu.s@blobcity.com>
2024-01-12 17:44:25 +0530

    added members

--- components/modals/edit-server.modal.tsx
+++ components/modals/edit-server.modal.tsx
@@ -25,6 +25,7 @@ import { FileUpload } from "@/components/file-upload";
 import axios from "axios"
 import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -35,27 +36,37 @@ const formSchema = z.object({
   }),
 });
 
-export const CreateServerModal = () => {
-  const {isOpen,onClose,type}=useModal();
+export const EditServerModal = () => {
+  const {isOpen,onClose,type,data}=useModal();
 
   const router = useRouter()
 
-  const isModalOpen = isOpen && type === "createServer";
-
+  const isModalOpen = isOpen && type === "editServer";
+  const {server}=data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
       imageUrl: "",
+
+
+
     },
   });
+
+  useEffect(()=>{
+    if(server){
+      form.setValue("name",server.name)
+      form.setValue("imageUrl",server.imageUrl);
+    }
+  },[server,form])
 
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try{
-        await axios.post("/api/servers",values);
+        await axios.patch(`/api/servers/${server?.id}`,values);
 
         form.reset();
         router.refresh();
@@ -127,7 +138,7 @@ export const CreateServerModal = () => {
             </div>
             <DialogFooter className="bg-gray-100 px-6 py-4">
               <Button variant="primary" disabled={isLoading}>
-                Create
+                Save
               </Button>
             </DialogFooter>
           </form>
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { CreateServerModal } from "@/components/modals/create-server-modal"
 import { useEffect, useState } from "react"
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server.modal";
+import { MembersModal } from "@/components/modals/members-modal";
 
 
 export const ModalProvider = ()=>{
@@ -14,10 +17,13 @@ export const ModalProvider = ()=>{
     if(!isMounted){
         return null;
     }
-    
+
     return(
         <>
         <CreateServerModal/>
+        <InviteModal/>
+        <EditServerModal/>
+        <MembersModal/>
         </>
     )
 }
